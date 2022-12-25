7589644afe1464a47a4bc77e266c8f5973f261ab
eniola-ak <114675566+eniola-ak@users.noreply.github.com>
2022-12-25 22:39:08 +0100

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function factorial(number) {
+  let prd = 1;
+  for (let i = number; i > 1; i--) {
+    // prd = product * i;
+
+    prd = prd * i;
+  }
+  console.log(product);
+}
+
+factorial(50);
