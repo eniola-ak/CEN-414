0d34ee52faedbfd6daaa9f8c83471ff7fcbb8992
eniola-ak <114675566+eniola-ak@users.noreply.github.com>
2022-12-25 22:41:06 +0100

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,16 @@
+def howmany(n):
+    n1, n2 = 0, 1
+    count = 0
+
+    if n <= 0:
+        print("Please enter a positive integer")
+    elif n == 1:
+        print(n1)
+    else:
+        while count < n:
+            print(n1)
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+print (howmany(100))
